0648f058309c261e8b2bb7292f7238adcd2ead35
Vovchik333 <vavan123music@gmail.com>
2024-02-09 14:56:22 +0200

    add user model

---
+++ src/data/database/models/user.model.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+import { type User } from "../../../common/types/user/user.type.js";
+
+const userSchema = new Schema<User>({
+    nickname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const userModel = model<User>('user', userSchema, 'users');
+
+export { userModel };
