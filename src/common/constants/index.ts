f457c7c18067f110dabcb2c989f327aae3cbe6d0
Volodymyr <vavan123music@gmail.com>
2024-08-22 19:10:09 +0300

    add constants for mongo and user

---
+++ src/common/constants/index.ts
@@ -0,0 +1,7 @@
+export {
+    MIN_PASSWORD_LENGTH,
+    MAX_PASSWORD_LENGTH,
+    MIN_NICKNAME_LENGTH,
+    MAX_NICKNAME_LENGTH
+} from './user/user.constants';
+export { OBJECT_ID_LENGTH } from './mongo/mongo.constants';
