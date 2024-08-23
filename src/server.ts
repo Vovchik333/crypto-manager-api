e9d57a6ff39f03c942f8ce2908d8a3d134a43274
Volodymyr <vavan123music@gmail.com>
2024-08-23 22:23:15 +0300

    fix imports in server file

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,7 @@
-import "dotenv/config";
-import express, { Express } from "express";
-import { router as routes } from './routes/routes.js';
-import { connector } from "./data/database/database.js";
-import { API_PORT } from "./config/config.js";
-import { ApiPath } from "./common/enums/api/api-path.enum.js";
-import { errorResponder } from "./middlewares/error/error-responder.middleware.js";
+import app from "./app";
+import { connector } from "@database";
 
-connector.connectToDB();
-
-const app: Express = express();
-
-app.use(express.json());
-app.use(ApiPath.API, routes);
-app.use(errorResponder);
-
-app.listen(API_PORT, () => {
-  console.log(`Server listening at http://localhost:${API_PORT}`);
-});
+(async () => {
+  await connector.connectToDB();
+  app.start();
+})();
