a3b37dd8c05068cd2415532dd620e72fb67f2cda
Volodymyr <vavan123music@gmail.com>
2024-08-25 17:22:20 +0300

    add transaction service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,13 +1,17 @@
-import { userRepository } from "../repositories";
+import { transactionRepository, userRepository } from "../repositories";
 import AuthService from "./auth/auth.service";
+import TransactionService from "./transaction/transaction.service";
 import UserService from "./user/user.service";
 
 const userService = new UserService(userRepository);
 const authService = new AuthService(userRepository);
+const transactionService = new TransactionService(transactionRepository);
 
 export { 
     userService,
     authService,
+    transactionService,
     AuthService,
-    UserService
+    UserService,
+    TransactionService
  };
