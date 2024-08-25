140d4417925109422f2ed059a1130118761e023a
Volodymyr <vavan123music@gmail.com>
2024-08-25 17:14:54 +0300

    add transaction types

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -7,3 +7,7 @@ export {
     type User, 
     type UserWithToken
 } from './user';
+export { 
+    type Transaction, 
+    type TransactionRequestData 
+} from './transaction';
---
+++ src/common/types/transaction/transaction.type.ts
@@ -0,0 +1,13 @@
+import { TransactionType } from "@enums";
+import { ValueOf } from "@generic";
+
+type Transaction = {
+    id: string;
+    assetId: string;
+    type: ValueOf<typeof TransactionType>;
+    pricePerCoin: number;
+    quantity: number;
+    createdAt?: string;
+}
+
+export { type Transaction };
