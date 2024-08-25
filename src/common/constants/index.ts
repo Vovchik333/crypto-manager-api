419130fa67705a901fb7e9911cdac846b249ba8d
Volodymyr <vavan123music@gmail.com>
2024-08-25 17:12:16 +0300

    add constants for transaction

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -4,4 +4,8 @@ export {
     MIN_NICKNAME_LENGTH,
     MAX_NICKNAME_LENGTH
 } from './user/user.constants';
+export {
+    MIN_PRICE_PER_COIN,
+    MIN_QUANTITY
+} from './transaction/transaction.constants';
 export { OBJECT_ID_LENGTH } from './mongo/mongo.constants';
---
+++ src/common/constants/transaction/transaction.constants.ts
@@ -0,0 +1,3 @@
+export const MIN_PRICE_PER_COIN = 0;
+
+export const MIN_QUANTITY = 0;
