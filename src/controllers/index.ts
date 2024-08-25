e7a7832937bf21c23fb487c1eb6fa4983989b3e8
Volodymyr <vavan123music@gmail.com>
2024-08-25 17:22:43 +0300

    add transaction controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,11 +1,14 @@
-import { authService, userService } from "../services";
+import { authService, transactionService, userService } from "../services";
 import AuthController from "./auth/auth.controller";
+import TransactionController from "./transaction/transaction.controller";
 import UserController from "./user/user.controller";
 
 const userController = new UserController(userService);
 const authController = new AuthController(authService);
+const transactionController = new TransactionController(transactionService);
 
 export { 
     userController,
-    authController
+    authController,
+    transactionController
 };
