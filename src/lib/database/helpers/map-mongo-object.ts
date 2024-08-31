9b90b8149f1d50e6c32675dd5820a41098967a85
Volodymyr <vavan123music@gmail.com>
2024-09-01 01:34:15 +0300

    added a null check for map-mongo-object

--- src/lib/database/helpers/map-mongo-object.ts
+++ src/lib/database/helpers/map-mongo-object.ts
@@ -1,6 +1,15 @@
+import { ErrorMessage } from "@enums";
+import { HttpCode, HttpError } from "@lib/services/http";
 import { Document } from "mongoose";
 
-const mapMongoObject = <T>(objectFromDb: T): T => {
+const mapMongoObject = <T>(objectFromDb: T | null): T => {
+    if (objectFromDb === null) {
+        throw new HttpError({
+            status: HttpCode.NOT_FOUND,
+            message: ErrorMessage.NOT_FOUND
+        });
+    }
+
     const { _id, __v, ...clearObject } = (objectFromDb as Document<T>).toObject();
 
     return {
