fb61fba6d4387c7988faa71abda4504d56f626b2
Volodymyr <vavan123music@gmail.com>
2024-09-01 01:36:23 +0300

    fix auth, transaction, and user routes

--- src/routes/transaction/transaction.service.ts
+++ src/routes/transaction/transaction.service.ts
@@ -1,72 +1,63 @@
-import { ErrorMessage } from "@enums";
-import { mapMongoObject } from "@lib/database";
 import { TransactionRepository } from "./transaction.repository";
 import { Transaction, TransactionRequestData } from "@types";
 import { UpdateQuery } from "mongoose";
-import { HttpCode, HttpError } from "@lib/services/http";
+import { AssetRepository } from "@routes/asset";
+import { TransactionType } from "@enums";
+import { mapMongoObject } from "@lib/database";
+
+type Constructor = {
+    transactionRepository: TransactionRepository;
+    assetRepository: AssetRepository;
+}
 
 class TransactionService {
     #transactionRepository: TransactionRepository;
+    #assetRepository: AssetRepository;
 
-    constructor(transactionRepository: TransactionRepository) {
+    constructor({ transactionRepository, assetRepository }: Constructor) {
         this.#transactionRepository = transactionRepository;
+        this.#assetRepository = assetRepository;
     }
 
-    public async create (payload: TransactionRequestData): Promise<Transaction> {
-        const transactionFromDb = await this.#transactionRepository.create(payload);
-        const transaction = mapMongoObject<Transaction>(transactionFromDb);
+    public async getByFilter(filter: Partial<Transaction>): Promise<Transaction[]> {
+        const transactions = await this.#transactionRepository.getByFilter(filter);
+
+        return transactions.map(mapMongoObject<Transaction>);
+    }
+
+    public async getById(id: string): Promise<Transaction> {
+        const transaction = mapMongoObject<Transaction>(await this.#transactionRepository.getById(id));
 
         return transaction;
     }
 
-    public async getByFilter(filter: Partial<Transaction>): Promise<Transaction[]> {
-        const transactionsFromDb = await this.#transactionRepository.getByFilter(filter);
-        const transactions = transactionsFromDb.map(mapMongoObject<Transaction>);
-
-        return transactions;
-    }
-
-    public async getById(id: string): Promise<Transaction> {
-        const transactionFromDb = await this.#transactionRepository.getById(id);
-
-        if (transactionFromDb === null) {
-            throw new HttpError({
-                status: HttpCode.NOT_FOUND,
-                message: ErrorMessage.NOT_FOUND
-            });
+    public async create (payload: TransactionRequestData): Promise<Transaction> {
+        if (payload.type === TransactionType.SELL && payload.quantity > 0) {
+            payload.quantity = -payload.quantity;
         }
 
-        const transaction = mapMongoObject<Transaction>(transactionFromDb);
+        const transaction = mapMongoObject<Transaction>(await this.#transactionRepository.create(payload));
+        await this.#assetRepository.addTransaction(transaction);
 
         return transaction;
     }
 
     public async updateById(id: string, payload: UpdateQuery<Transaction>): Promise<Transaction> {
-        const transactionFromDb = await this.#transactionRepository.updateById(id, payload);
+        const prevTransaction = mapMongoObject<Transaction>(await this.#transactionRepository.getById(id));
 
-        if (transactionFromDb === null) {
-            throw new HttpError({
-                status: HttpCode.NOT_FOUND,
-                message: ErrorMessage.NOT_FOUND
-            });
+        if (prevTransaction.type === TransactionType.SELL && payload.quantity > 0) {
+            payload.quantity = -payload.quantity;
         }
 
-        const transaction = mapMongoObject<Transaction>(transactionFromDb);
+        const transaction = mapMongoObject<Transaction>(await this.#transactionRepository.updateById(id, payload));
+        await this.#assetRepository.updateTransaction(prevTransaction, transaction);
 
         return transaction;
     }
 
     public async deleteById(id: string): Promise<Transaction> {
-        const transactionFromDb = await this.#transactionRepository.deleteById(id);
-
-        if (transactionFromDb === null) {
-            throw new HttpError({
-                status: HttpCode.NOT_FOUND,
-                message: ErrorMessage.NOT_FOUND
-            });
-        }
-
-        const transaction = mapMongoObject<Transaction>(transactionFromDb);
+        const transaction = mapMongoObject<Transaction>(await this.#transactionRepository.deleteById(id));
+        await this.#assetRepository.removeTransaction(transaction);
 
         return transaction;
     }
